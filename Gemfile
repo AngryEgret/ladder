347ac489a652207f1054c571d3418cba39ef33f2
Ryan Greget <ryan.greget@lookout.com>
2014-04-09 11:06:25 -0700

    rails new

--- Gemfile
+++ Gemfile
@@ -1,13 +1,15 @@
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-ruby "1.9.3"
+gem 'rails', '~>4.0.4'
+gem 'sqlite3'
+gem 'sass-rails', '~> 4.0.2'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
 
-gem 'elo'
-gem 'trueskill', :git => 'git://github.com/saulabs/trueskill.git', :require => "saulabs/trueskill"
-
-group :test, :development do
-  gem 'rake'
-  gem 'rspec'
-  gem 'rack-test'
-  gem 'coveralls', require: false
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
 end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ladder::Application.config.secret_key_base = 'a5610a467fcbe463ad1ecf863a7facf5c026ab79baf556ec54d4428c8641377219cc5d1d63c9029a624eba39f63e58ba6155d1b39e9f9b5a4bc79319c4ec00f3'
